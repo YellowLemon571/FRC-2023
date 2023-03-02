cdde4a2f9aef56f4b172741e160778fb38769674
tyra <tyragipson50@gmail.com>
2023-03-02 11:28:44 -0600

    fixed claw and lift commands

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -1,12 +1,11 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Claw extends SubsystemBase{
-  private PWMTalonFX Claw = new PWMTalonFX(6);
+  private PWMTalonFX claw = new PWMTalonFX(6);
 
   public CommandBase openClaw(){
     return this.run(()  -> claw.set(0.5));
--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 //for command functions
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -12,10 +13,9 @@ public class Lift extends SubsystemBase {
   //says what port the motor is plugged into
   private PWMTalonFX rightLift = new PWMTalonFX(4);
   private PWMTalonFX leftLift = new PWMTalonFX(5);
-
-  public final lift Lift =
-        new lift(rightLift, leftLift);
-
+  
+  MotorControllerGroup Lift = new MotorControllerGroup(leftLift, rightLift);
+ 
   public CommandBase  raiseLift() {
         return this.run(() -> Lift.set(-0.25));
     }
